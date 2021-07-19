52d2a090976c235463a93c8f3a0248438be3a9e8
slavo3dev <slavo@slavo3.com>
2021-07-19 13:49:35 -0400

    adding getallproducts

---
+++ framework/shopify/products/get-all-products.ts
@@ -0,0 +1,4 @@
+export const getAllProducts = async (): Promise<number[]> => {
+  const products = [1, 2, 3];
+  return products;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { InferGetStaticPropsType } from "next";
+import { getAllProducts } from "../framework/shopify/products/get-all-products";
 
 export async function getStaticProps() {
-  const products = [1, 2, 3];
+  const products = await getAllProducts();
 
   return {
     props: {
